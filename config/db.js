7f84c3cbba30b601ae7e98009b81176e5f51b7f0
Lubangula <danielmayombo47@gmail.com>
2025-01-28 22:13:08 +0100

    mongodbAtlas

--- config/db.js
+++ config/db.js
@@ -1,30 +1,14 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-
-// // Charger l'URI de MongoDB depuis .env
-// const mongoURI = process.env.MONGO_URI
-
-// // Fonction de connexion à MongoDB
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(mongoURI);
-//         console.log('MongoDB connecté');
-//     } catch (err) {
-//         console.error('Erreur de connexion à MongoDB', err);
-//         process.exit(1);  // Quitter l'application en cas d'échec de la connexion
-//     }
-// };
-
-// module.exports = { connect: connectDB };
-
-
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+// Charger l'URI de MongoDB depuis .env
+const mongoURI = process.env.MONGO_URI
 
 // Fonction de connexion à MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/site-vente');
+        await mongoose.connect(mongoURI);
         console.log('MongoDB connecté');
     } catch (err) {
         console.error('Erreur de connexion à MongoDB', err);
@@ -33,3 +17,19 @@ const connectDB = async () => {
 };
 
 module.exports = { connect: connectDB };
+
+
+// const mongoose = require('mongoose');
+
+// // Fonction de connexion à MongoDB
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect('mongodb://localhost/site-vente');
+//         console.log('MongoDB connecté');
+//     } catch (err) {
+//         console.error('Erreur de connexion à MongoDB', err);
+//         process.exit(1);  // Quitter l'application en cas d'échec de la connexion
+//     }
+// };
+
+// module.exports = { connect: connectDB };
